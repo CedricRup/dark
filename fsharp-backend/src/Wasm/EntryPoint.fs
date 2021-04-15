cb56e05edbe64c5cd60f6a4c53448c32b4fb06a1
Cédric Rup <cedric.rup@gmail.com>
2021-04-15 21:59:18 +0200

    Blazor webworker

---
+++ fsharp-backend/src/Wasm/EntryPoint.fs
@@ -0,0 +1,10 @@
+namespace Wasm
+open Microsoft.AspNetCore.Components.WebAssembly.Hosting
+
+module Program =
+  
+ [<EntryPoint>]
+  let Main args =
+    let builder = WebAssemblyHostBuilder.CreateDefault([||])
+    builder.Build().RunAsync() |> ignore
+    0
